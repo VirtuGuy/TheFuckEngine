f50107be3ab2a2602c9b8a43154428a20cae8116
Macohi <sinconsistencia@gmail.com>
2026-02-15 09:22:07 -0600

    I underestimated the requirements of my crash handler class

---
+++ source/macohi/util/StringUtil.hx
@@ -0,0 +1,46 @@
+package macohi.util;
+
+import lime.utils.Assets as KoyaAssets; // import macohi.funkin.koya.backend.KoyaAssets;
+
+using StringTools;
+
+class StringUtil
+{
+	/**
+	 * Checks if a string is `null` or blank (contains only whitespaces).
+	 * 
+	 * Yoinked from an FNF pr heheheehehehee
+	 * https://github.com/FunkinCrew/Funkin/pull/6910
+	 *
+	 * @param value The string to check.
+	 * @return True... or False...
+	 */
+	public static function isBlank(value:String):Bool
+	{
+		return value == null || value.trim().length == 0;
+	}
+
+	public static function splitStringByNewlines(str:String):Array<String>
+	{
+		if (str == null)
+			return [];
+
+		var strA:Array<String> = [];
+
+		for (entry in str.split('\n'))
+			strA.push(entry.trim());
+
+		return strA;
+	}
+
+	public static function splitTextAssetByNewlines(txt:String)
+	{
+		if (!KoyaAssets.exists(txt))
+		{
+			WindowUtil.alert('Missing Path: $txt', '$txt is a missing path\nand thus cannot be split by newlines');
+			return [];
+		}
+
+		return splitStringByNewlines(KoyaAssets.getText(txt));
+	}
+}
