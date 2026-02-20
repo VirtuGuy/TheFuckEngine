81cdff081906ca15068dac55755f8d271bfce919
VirtuGuy <103977942+VirtuGuy@users.noreply.github.com>
2026-02-19 21:35:43 -0500

    Remove mp3s
    Mp3s were only part of the engine because of web. Due to the fact web is no more, I decided to remove every mp3 file from the game.

--- source/funkin/util/Constants.hx
+++ source/funkin/util/Constants.hx
@@ -6,7 +6,7 @@ package funkin.util;
 class Constants
 {
     public static final IMAGE_EXT:String = 'png';
-    public static final SOUND_EXT:String = #if web 'mp3' #else 'ogg' #end;
+    public static final SOUND_EXT:String = 'ogg';
     public static final JSON_EXT:String = 'json';
 
     public static final MS_PER_SEC:Int = 1000;
