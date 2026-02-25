0fbd70828400df3f8cdb31ca705eab6b4d15bd29
Macohi <sinconsistencia@gmail.com>
2026-02-24 21:12:20 -0600

    freaky menu + add tite gf

--- source/funkin/ui/title/TitleScreen.hx
+++ source/funkin/ui/title/TitleScreen.hx
@@ -1,10 +1,32 @@
 package funkin.ui.title;
 
+import flixel.FlxG;
+import funkin.data.character.CharacterRegistry;
+import funkin.play.Character;
+
 class TitleScreen extends FunkinState
 {
+	var titleGF:Character;
+
 	override function create()
 	{
 		super.create();
+
+		titleGF = CharacterRegistry.instance.fetchCharacter('title-gf');
+		add(titleGF);
+		titleGF.screenCenter();
+
+		conductor.bpm = 102;
+		conductor.time = 0;
+		FlxG.sound.playMusic(Paths.sound('music/freakyMenu'), 1, true);
+		
+	}
+
+	override function beatHit(beat:Int)
+	{
+		super.beatHit(beat);
+
+		titleGF.dance();
 	}
 
 	override function update(elapsed:Float)
