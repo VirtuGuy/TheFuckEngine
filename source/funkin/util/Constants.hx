a16453616de285cfc43c84c30e04a14e6863bc73
realvirtu <103977942+realvirtu@users.noreply.github.com>
2026-08-16 13:34:09 -0400

    Music ease ratio adjustments

--- source/funkin/util/Constants.hx
+++ source/funkin/util/Constants.hx
@@ -24,7 +24,6 @@ class Constants
 
 	public static final NOTE_COUNT:Int = 4;
 	public static final ZOOM:Float = 1.35;
-	public static final MUSIC_EASE_RATIO:Float = 20;
 
 	public static final DEFAULT_NAME:String = 'Untitled';
 	public static final DEFAULT_ARTIST:String = 'Unknown';
@@ -41,6 +40,7 @@ class Constants
 
 	public static final CONDUCTOR_DRIFT_THRESHOLD:Float = 65;
 	public static final RESYNC_THRESHOLD:Float = 30;
+	public static final MUSIC_EASE_RATIO:Float = 42;
 
 	public static final HIT_WINDOW_MS:Float = 160;
 	public static final SICK_WINDOW_MS:Float = 45;
