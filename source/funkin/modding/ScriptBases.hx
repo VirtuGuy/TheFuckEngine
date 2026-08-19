05843e1d3e3d1b34a3d2c428a7909f044ce8f114
realvirtu <103977942+realvirtu@users.noreply.github.com>
2026-08-19 08:15:51 -0400

    Bump polymod
    Scripts are now able to extend anything and everything, so that's why most of the script bases were removed

--- source/funkin/modding/ScriptBases.hx
+++ source/funkin/modding/ScriptBases.hx
@@ -19,9 +19,6 @@ class ScriptedCharacter extends funkin.play.character.Character implements HScri
 class ScriptedStage extends funkin.play.stage.Stage implements HScriptedClass {}
 
 @:hscriptClass
-class ScriptedStageProp extends funkin.play.stage.StageProp implements HScriptedClass {}
-
-@:hscriptClass
 class ScriptedSongEvent extends funkin.play.song.SongEvent implements HScriptedClass {}
 
 @:hscriptClass
@@ -31,9 +28,6 @@ class ScriptedNoteKind extends funkin.play.note.NoteKind implements HScriptedCla
 class ScriptedNoteStyle extends funkin.play.note.NoteStyle implements HScriptedClass {}
 
 @:hscriptClass
-class ScriptedBaseCutscene extends funkin.play.cutscene.BaseCutscene implements HScriptedClass {}
-
-@:hscriptClass
 class ScriptedStickerPack extends funkin.ui.sticker.StickerPack implements HScriptedClass {}
 
 @:hscriptClass
@@ -41,59 +35,3 @@ class ScriptedAlbum extends funkin.ui.freeplay.album.Album implements HScriptedC
 
 @:hscriptClass
 class ScriptedModule extends funkin.modding.module.Module implements HScriptedClass {}
-
-//
-// MISC
-//
-
-@:hscriptClass
-class ScriptedFunkinBar extends funkin.graphics.FunkinBar implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFunkinSprite extends funkin.graphics.FunkinSprite implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFunkinText extends funkin.graphics.FunkinText implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFunkinState extends funkin.ui.FunkinState implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFunkinSubState extends funkin.ui.FunkinSubState implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedMenuList extends funkin.ui.TextMenuList implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedStateMachine extends funkin.ui.StateMachine implements HScriptedClass {}
-
-//
-// FLIXEL
-//
-
-@:hscriptClass
-class ScriptedFlxCamera extends flixel.FlxCamera implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxSprite extends flixel.FlxSprite implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxState extends flixel.FlxState implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxSubState extends flixel.FlxSubState implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxTypedGroup extends flixel.group.FlxGroup.FlxTypedGroup<Dynamic> implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxTypedSpriteGroup extends flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup<Dynamic> implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxSound extends flixel.sound.FlxSound implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxText extends flixel.text.FlxText implements HScriptedClass {}
-
-@:hscriptClass
-class ScriptedFlxBitmapText extends flixel.text.FlxBitmapText implements HScriptedClass {}
